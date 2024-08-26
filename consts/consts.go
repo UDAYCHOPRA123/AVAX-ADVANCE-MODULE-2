606cf98e85c20ab57ea3311455abf8370fb69cc0
UDAY CHOPRA <udayhero02@gmail.com>
2024-08-26 20:59:29 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = "Rizz"
+	HRP = "Meta"
 	// TODO: choose a name for your hyperchain
-	Name = "MetaRizz"
+	Name = "Metacrafter"
 	// TODO: choose a token symbol
-	Symbol = "MRZ"
+	Symbol = "MetaC"
 )
 
 var ID ids.ID
